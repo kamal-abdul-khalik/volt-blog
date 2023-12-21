81b510966f21650a4768fc604ca00e453aa20735
kamal-abdul-khalik <elsakcf@gmail.com>
2023-12-21 15:55:09 +0800

    wip fix double quote

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,9 +1,9 @@
-@props(['active'])
+@props(["active"])
 
 @php
-    $classes = $active ?? false ? 'inline-flex flex items-center space-x-2 text-sm font-bold text-yellow-500 hover:text-yellow-900 transition duration-150 ease-in-out' : 'inline-flex flex items-center space-x-2 text-sm text-yellow-500 hover:text-yellow-900 transition duration-150 ease-in-out';
+    $classes = $active ?? false ? "inline-flex flex items-center space-x-2 text-sm font-bold text-yellow-500 hover:text-yellow-900 transition duration-150 ease-in-out" : "inline-flex flex items-center space-x-2 text-sm text-yellow-500 hover:text-yellow-900 transition duration-150 ease-in-out";
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }} wire:navigate>
+<a {{ $attributes->merge(["class" => $classes]) }} wire:navigate>
     {{ $slot }}
 </a>
